e0aaa0dbd947ae3a7553a9ed6334319637f53fa0
g1YC31aA <lennartmobil2016@gmail.com>
2019-05-10 20:10:33 +0200

    added save_as_image call

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import knn
 
 if __name__ == '__main__':
-    for i in range(50):
-        knn.perform_knn(7,"../data/mnist_test.csv",i+10,"../data/mnist_train.csv")
+    for i in range(100 ):
+        knn.perform_knn(7,"../data/mnist_test.csv",i+1,"../data/mnist_train.csv")
+    image_operations.save_as_image("../data/mnist_train.csv", 14)
