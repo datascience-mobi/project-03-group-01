d085b08cafa68692fc49c2c08ecd7532faff41dc
Lukas <l.voos@stud.uni-heidelberg.de>
2019-05-22 10:40:58 +0200

    Merge branch 'image_as_object' of C:\Users\lukas\PycharmProjects\project-03-group-01 with conflicts.

---
+++ src/plot.py
@@ -0,0 +1,28 @@
+import matplotlib as mlp
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_values (input_list):
+    """for training purposes"""
+
+a = [[1, 7], [2, 8], [3, 0], [4, 9], [5, 12]]
+
+labels, ys = zip(*a)
+xs = np.arange(len(labels))
+width = 0.8
+
+plt.bar(xs, ys, width, align='center')
+
+plt.xticks(xs, labels) #Replace default x-ticks with xs, then replace xs with labels
+plt.yticks(ys)
+
+plt.ylabel('Accuracy')
+plt.xlabel('#k')
+plt.title('Accuracy test')
+
+plt.show()
+
+if __name__ == '__main__':
+    a = [[1, 7], [2, 8], [3, 0], [4, 9], [5, 12]]
+    plot_values(a)
