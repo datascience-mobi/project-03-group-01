3b3d7a12b990a3297cbb4e27d3d4f9df70fcb8ee
Lukas <l.voos@stud.uni-heidelberg.de>
2019-05-22 10:07:57 +0200

    error correction

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import knn, pca, image_operations
+import knn, pca, image_operations, plot
 
 if __name__ == '__main__':
     #for i in range(5):
      #   knn.perform_knn(7,"../data/mnist_test.csv",i+1,"../data/mnist_train.csv")
     #image_operations.save_as_image("../data/mnist_train.csv", 14)
-    pca.perform_pca("../data/mnist_train.csv")
+    #pca.perform_pca("../data/mnist_train.csv")
+    a = [[1, 7], [2, 8], [3, 0], [4, 9], [5, 12]]
+    plot.plot_values(a)
--- src/plot.py
+++ src/plot.py
@@ -6,22 +6,20 @@ import numpy as np
 def plot_values (input_list):
     """for training purposes"""
 
-a = [[1, 7], [2, 8], [3, 0], [4, 9], [5, 12]]
+    labels, ys = zip(*a)
+    xs = np.arange(len(labels))
+    width = 0.8
 
-labels, ys = zip(*a)
-xs = np.arange(len(labels))
-width = 0.8
+    plt.bar(xs, ys, width, align='center')
 
-plt.bar(xs, ys, width, align='center')
+    plt.xticks(xs, labels) #Replace default x-ticks with xs, then replace xs with labels
+    plt.yticks(ys)
 
-plt.xticks(xs, labels) #Replace default x-ticks with xs, then replace xs with labels
-plt.yticks(ys)
+    plt.ylabel('Accuracy')
+    plt.xlabel('#k')
+    plt.title('Accuracy test')
 
-plt.ylabel('Accuracy')
-plt.xlabel('#k')
-plt.title('Accuracy test')
-
-plt.show()
+    plt.show()
 
 if __name__ == '__main__':
     a = [[1, 7], [2, 8], [3, 0], [4, 9], [5, 12]]
