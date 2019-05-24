ea8c93f5e87e88564b4bf2f7bd44f946743a0b12
g1YC31aA <lennartmobil2016@gmail.com>
2019-05-24 21:09:02 +0200

    included function calls for image_operations

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import knn, pca, image_operations, load_image_vectors
+import src.knn as knn
+# import src.pca as pca
+import src.image_operations as image_operations
+import src.load_image_vectors as load_image_vectors
 
 tests_count = 0
 tests_success = 0
@@ -20,17 +23,21 @@ def get_success_rate ():
 
 if __name__ == '__main__':
     k = 20  # number of nearest neighbors to check
-    training_gz = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
-    training_list = load_image_vectors.get_image_object_list(training_gz)
+    #training_gz = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
+    #training_list = load_image_vectors.get_image_object_list(training_gz)
     print("Successfully loaded training list")
     test_gz = load_image_vectors.load_gz('../data/mnist_test.csv.gz')
     test_list = load_image_vectors.get_image_object_list(test_gz)
     print("Successfully loaded test list")
 
+    image_operations.draw(test_list[1].image)
+    image_operations.save('../fname.png', test_list[1].image)
+
+
     # training_csv = load_image_vectors.load_csv('../data/mnist_train.csv') # Alternative to load_gz
-    for i in range (10):
-        sorted_distances = knn.get_sorted_distances(test_list[i], training_list)
-        # print("Successfully calculated distance of one test image to all training images")
-        predicted_digit = knn.knn_distance_prediction(sorted_distances,k)
-        set_success_rate(predicted_digit, test_list[i])
-    print("Success rate: " + str(get_success_rate()))
+    #for i in range (10):
+    #    sorted_distances = knn.get_sorted_distances(test_list[i], training_list)
+    #    # print("Successfully calculated distance of one test image to all training images")
+    #    predicted_digit = knn.knn_distance_prediction(sorted_distances,k)
+    #    set_success_rate(predicted_digit, test_list[i])
+    #print("Success rate: " + str(get_success_rate()))
