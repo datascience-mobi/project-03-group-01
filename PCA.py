07b8118b8d269517ea12c20c8b81fa87d04df252
Dzianis <denisyar99@gmail.com>
2019-05-28 00:17:51 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -5,13 +5,17 @@ from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import matplotlib as plt
 
-genes = ["gene" + str(i) for i in range (1,101)]
+genes = ['gene' + str(i) for i in range (1,101)]
 
-wt= ["wt"+str (i) for i in range (1,6)]
-ko = ["ko" + str (i) for i in range(1,6)]
+wt= ['wt'+str (i) for i in range (1,6)]
+ko = ['ko' + str (i) for i in range(1,6)]
 
 data = pd.DataFrame (columns = [*wt, *ko], index = genes)
-data.loc [gene,"wt1" :'wt5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
-data.loc [gene,"ko1" :'ko5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
+data.loc [genes,'wt1' :'wt5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
+data.loc [genes,'ko1':'ko5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
 print (data.head())
 print(data.shape)
+scaled_data = preprocessing.scale(data.T)
+pca= PCA()
+pca.fit(scaled_data)
+pca_data=pca.transform(scaled_data)
