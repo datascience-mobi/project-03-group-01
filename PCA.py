e051bfc17204de7c61a502bb1c13ab043e86c1d0
Dzianis <denisyar99@gmail.com>
2019-05-28 00:02:33 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -5,12 +5,13 @@ from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import matplotlib as plt
 
-genes = ["gene" + str(i) in range (1,101)]
+genes = ["gene" + str(i) for i in range (1,101)]
 
-wt= ["wt"+str (i) in range (1,6)]
-ko = ["ko" + str (i) in range(1,6)]
+wt= ["wt"+str (i) for i in range (1,6)]
+ko = ["ko" + str (i) for i in range(1,6)]
 
-data = pd.dataframe (columns = [*wt, *ko], index = genes)
+data = pd.DataFrame (columns = [*wt, *ko], index = genes)
 data.loc [gene,"wt1" :'wt5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
 data.loc [gene,"ko1" :'ko5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
 print (data.head())
+print(data.shape)
