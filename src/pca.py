bd2022cb69286365f4e95cf081858191c68e5a22
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-04 19:49:29 +0200

    switched to sklearning

--- src/pca.py
+++ src/pca.py
@@ -1,94 +1,79 @@
-import shutil, os
+import pandas as pd
+import numpy as np
+import random as rd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
-def perform_pca(training_path):
-    """For normalization: Get vactor containing mean values for all pixels and training images"""
-    sumintensities=[]
-    meanintensities=[]
-    numimages=0
-    with open(training_path) as infile:
-        for line in infile:
-            image_vector=line.split(",")
-            sumintensities=[0]*len(image_vector)
-            meanintensities=[0]*len(image_vector)
-    truedigits=[]
-    """with open(training_path) as infile:
-        for line in infile:
-            image_vector=line.split(",")
-            truedigits.append(int(image_vector[0]))
-            image_vector.pop(0)
-            for i in range(len(image_vector)):
-                sumintensities[i]+=int(image_vector[i])
-            numimages+=1
-        for index in range(len(sumintensities)):
-            meanintensities[index]=int(round(float(sumintensities[index])/float(numimages)))
-        print(meanintensities)
-        print("Mean list created")
-    shutil.rmtree("../temp", ignore_errors=True)
-    os.mkdir("../temp")
-    f = open("../temp/normalized_training.csv", "w+")
-    with open(training_path) as infile:
-        linenumber=0
-        for line in infile:
-            f.write(str(truedigits[linenumber])+",")
-            linenumber+=1
-            old_vector=line.split(",")
-            for index in range(len(old_vector)):
-                f.write(str(int(old_vector[index])-meanintensities[index])+",")
-            f.write("\n")
-    f.close()
-    print("Normalizing completed")"""
 
-    """Create covariance matrix of our X*N Matrix where X=pixelnumber and N=image number
-        with the formula of Sigma(i,j)=1/numrows-1 * crossproduct (coli, colj)"""
-    covariance_matrix=[]
-    templist3=[]
-    f=open("../temp/covmatrix_training.csv", "w+")
-    for i in range(50,len(sumintensities)):
-        templist=collist(i, training_path)
-        for j in range(50,len(sumintensities)):
-            templist2=collist(j, training_path)
-            #print("Sizes: "+str(len(templist))+" and "+str(len(templist2)))
-            cov=covariance(len(sumintensities), templist, templist2)
-            templist3.append(cov)
-            print(cov)
-            print(str(i)+" x "+str(j))
-        #covariance_matrix.append(templist3)
-        print(templist3)
-        for t3 in templist3:
-            f.write(str(t3)+",")
-        f.write("\n")
-        print(templist3)
-    print("Covariance matrix finished!")
-    f.close()
+def prepare_data(train_list, test_list):
+    scaler = preprocessing.StandardScaler()
+    #print(train_list)
+    # Fit on training set only.
+    scaler.fit(train_list)
 
-def collist(colnumber, path):
-    templist=[]
-    with open(path) as infile:
-        for line in infile:
-            linelist=line.split(",")
-            num=0
-            for l in range(len(linelist)):
-                if num == colnumber:
-                    templist.append(int(linelist[l]))
-                    break
-                num += 1
-    #print("Scale: "+str(len(templist))+"   "+str(colnumber))
-    return templist
+    # Apply transform to both the training set and the test set.
+    train_list = scaler.transform(train_list)
+    test_list = scaler.transform(test_list)
 
-def covariance(n, list1, list2):
-    print("n: "+str(n))
-    factor1=1.0/float(n-1)
-    print(".......÷÷÷÷÷÷÷÷÷ "+str(factor1))
-    cov=factor1*cross_product(list1, list2)
-    print("-- "+str(cov))
-    return cov
+    #print(train_img)
 
-def cross_product(list1, list2):
-    if not len(list1) == len(list2):
-        print("Incompatible types: "+str(len(list1))+" and "+str(len(list2)))
-        return -1
-    product=0
-    for index in range(len(list1)):
-        product+=list1[index]*list2[index]
-    print("Product: "+str(product))
-    return product
+    pca = PCA(.95) # how much variance to keep
+    pca.fit(train_list)
+    print(pca.n_components_)
+    train_list = pca.transform(train_list)
+    test_list = pca.transform(test_list)
+    for i in range(5):
+        print(len(train_list[i]))
+    print(len(train_list))
+    print(len(test_list))
+    return train_list, test_list
+
+def reduce_dim():
+
+    genes = ['image' + str(i) for i in range (1,101)]
+    # Output: ['gene1', ..., 'gene100']
+    #print(genes)
+
+    wt= ['pixel'+str (i) for i in range (1,785)]
+    #ko = ['ko' + str (i) for i in range(1,6)]
+    # Output ['ko1', ... , 'ko5']
+
+    data = pd.DataFrame (columns = [*wt], index=genes)
+    for gene in data.index:
+        data.loc[gene,'pixel1':'pixel784'] = np.random.poisson(lam=rd.randrange(10,1000), size=784)
+        #data.loc[gene,'ko1':'ko5'] = np.random.poisson(lam=rd.randrange(10,1000), size=5)
+    #     data.loc [genes,'wt1' :'wt5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
+    #     data.loc [genes,'ko1':'ko5']= np.random.normal(loc=rd.randrange(10,1000), scale=rd.randint(1,100), size=5)
+    # print (data.head())
+    print(data.shape)
+    print(data.head())
+    scaled_data = preprocessing.scale(data.T)
+    pca = PCA()
+    pca.fit(scaled_data)
+    pca_data=pca.transform(scaled_data)
+    variation=np.round(pca.explained_variance_ratio_*100,decimals=1)
+    labels=('PCA'+str(i) for i in range(1,len(variation)+1))
+
+    plt.bar(x=range(1,len(variation)+1), height=variation)
+    plt.ylabel('Percentage of Explained Variance')
+    plt.xlabel('Principal Component')
+    plt.title('Scree Plot')
+    plt.show()
+
+    pca_df = pd.DataFrame(pca_data, index=[*wt], columns=labels)
+    print (pca_df.head())
+    print(pca_df.shape)
+    plt.scatter(pca_df.PCA1, pca_df.PCA2)
+    plt.title('My PCA Graph')
+    plt.xlabel('PC1 - {0}%'.format(variation[0]))
+    plt.ylabel('PC2 - {0}%'.format(variation[1]))
+
+    for sample in pca_df.index:
+        plt.annotate(sample, (pca_df.PCA1.loc[sample], pca_df.PCA2.loc[sample]))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    reduce_dim()
