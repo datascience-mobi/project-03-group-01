9595fca0fa1688f6a33538c71ff82da12d2d7620
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-05 09:42:28 +0200

    load functions directly return CsvImage objects

--- src/load_image_vectors.py
+++ src/load_image_vectors.py
@@ -1,5 +1,5 @@
 import gzip
-from typing import List # TODO: why?
+from typing import List
 
 
 def load_gz(path) -> list:
@@ -15,24 +15,25 @@ def load_gz(path) -> list:
     finally:
         input_file.close()
 
-    """ Translate data (byte) to data (str) """
-    data=data.decode("utf-8")
+    # Translate data (byte) to data (str)
+    data = data.decode("utf-8")
 
-    """ Create list, where each element is one image """
-    data_vector=data.split("\n")
+    # Create list, where each element is one image
+    data_vector = data.split("\n")
 
-    """ Make sure not to return empty or corrupted lines - optional """
-    """count=0
-    for dat in datavector:
-        if not (len(dat.split(",")))==785:
-            print("FAIL"+str(count))
-        count+=1"""
+    # Make sure not to return empty or corrupted lines - optional
+    # count=0
+    # for dat in data_vector:
+    #     if not (len(dat.split(",")))==785:
+    #         print("FAIL"+str(count))
+    #     count+=1
 
-    """ Remove empty last element, was created since the input string ends with "\n" """
+    # Remove empty last element, was created since the input string ends with "\n"
     data_vector.pop(len(data_vector) - 1)
     print(len(data_vector))
-    
-    return data_vector
+
+    # directly return loaded image as CsvImage object list
+    return get_image_object_list(data_vector)
 
 
 def load_csv(path) -> list:
@@ -43,16 +44,17 @@ def load_csv(path) -> list:
     """
     data_list = list()  # type: List[str]
 
-    """ Add each line from input .csv to data_list """
+    # Add each line from input .csv to data_list
     with open(path) as infile:
         for line in infile:
             line = line.replace("\n", "")
             data_list.append(line)
 
-    print(len(data_list)) # Check if line count is as expected
+    print(len(data_list))  # Check if line count is as expected
     # print(data_list[0]) # Check if "\n" successfully removed
 
-    return data_list
+    # directly return as CsvImage object list
+    return get_image_object_list(data_list)
 
 
 def get_image_object_list(data_list):
@@ -73,8 +75,6 @@ class CsvImage:
     """
     Contains the images label (which digit it represents) and its list of intensity values
     """
-    #label = None # TODO: static variables?
-    #image = list()
 
     def __init__(self, input_image):
         """
