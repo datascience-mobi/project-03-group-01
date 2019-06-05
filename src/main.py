647e80deda14f7205320b29102e31073c057d795
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-05 09:38:30 +0200

    adjusted to modified load_image_vectors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import knn, pca, image_operations, load_image_vectors
+import src.knn as knn
+import src.load_image_vectors as load_image_vectors
 
 tests_count = 0
 tests_success = 0
@@ -12,7 +13,7 @@ def set_success_rate(prediction, test_image) -> None:
     return None
 
 
-def get_success_rate ():
+def get_success_rate():
     global tests_count, tests_success
     success = float(tests_success)/float(tests_count)
     return round(success, 4)
@@ -20,17 +21,12 @@ def get_success_rate ():
 
 if __name__ == '__main__':
     k = 20  # number of nearest neighbors to check
-    training_gz = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
-    training_list = load_image_vectors.get_image_object_list(training_gz)
+    training_list = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
     print("Successfully loaded training list")
-    test_gz = load_image_vectors.load_gz('../data/mnist_test.csv.gz')
-    test_list = load_image_vectors.get_image_object_list(test_gz)
+    test_list = load_image_vectors.load_gz('../data/mnist_test.csv.gz')
     print("Successfully loaded test list")
 
-    # training_csv = load_image_vectors.load_csv('../data/mnist_train.csv') # Alternative to load_gz
-    for i in range (10):
-        sorted_distances = knn.get_sorted_distances(test_list[i], training_list)
-        # print("Successfully calculated distance of one test image to all training images")
-        predicted_digit = knn.knn_distance_prediction(sorted_distances,k)
-        set_success_rate(predicted_digit, test_list[i])
-    print("Success rate: " + str(get_success_rate()))
+    sorted_distances = knn.get_sorted_distances(test_list[8], training_list)
+    print("Successfully calculated distance of one test image to all training images")
+    predicted_digit = knn.knn_distance_prediction(sorted_distances, k)
+    print(predicted_digit, test_list[8].label)
