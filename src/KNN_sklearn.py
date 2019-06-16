1d64837a5cd6598b48c05a79479b33806b9bba37
Dzianis <denisyar99@gmail.com>
2019-06-17 00:26:39 +0200

    ave Denis

--- src/KNN_sklearn.py
+++ src/KNN_sklearn.py
@@ -2,18 +2,27 @@
 import sklearn
 
 import src.load_image_vectors as load_image_vectors
+from sklearn import neighbors, metrics
+
+"the function takes two tuples: train and test; N_neighbours for KNN; M_labels for output "
+"it reads the labels of test images and creates the 784 dim. space,"
+"in which the points from test_tuple will be entered."
+" then the KNN using N nighbours  with the points from test_tuple will be performed"
+"output - M_labels of test_tuple"
 
 
-# noinspection PyUnresolvedReferences
-def knn_sk(test_tuple, train_tuple):
+def knn_sk(test_tuple, train_tuple, n_neighbours, m_labels):
     train_labels = list()
+    test_pred = list()
     for i in range(len(train_tuple[0])):
-        train_labels.append = train_tuple[0][i].label
+        train_labels.append(train_tuple[0][i].label)
     #print (train_lists[:10][:10])
     # print (train_labels[:10])
-    knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=3).fit(train_tuple[1], train_label)
+    knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=n_neighbours).fit(train_tuple[1], train_labels)
     # print(knn.predict(test_list[:10]))
-    test_prediction = knn.predict(test_tuple)
+    for j in range(m_labels):
+        test_pred.append(test_tuple[1][j])
+    test_prediction = knn.predict(test_pred)
     return test_prediction
     # return type (train_labels), type (test_lists), type (train_lists)
 
@@ -34,7 +43,7 @@ print("Successfully loaded test list")
 # print(dim(test_lists))
 
 
-print(knn_sk(test_lists, training_lists))
+print(knn_sk(test_lists, training_lists, 3, 10))
 #print (type(training_lists))
 #def read_idx(filename):
 
