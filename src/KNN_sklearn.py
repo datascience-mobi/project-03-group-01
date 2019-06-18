7f8554842e8e839a7f31c02ed20e03299ad66c62
SakulV <49947770+SakulV@users.noreply.github.com>
2019-06-18 17:21:47 +0200

    moved the k_value_function to its own branch, cleaned up the messy knn_sk function and still cannot test it

--- src/KNN_sklearn.py
+++ src/KNN_sklearn.py
@@ -1,22 +1,19 @@
 # from pylab import reParams
 import sklearn
 
-import src.load_image_vectors as load_image_vectors
-from sklearn import neighbors, metrics
-
-"the function takes two tuples: train and test; N_neighbours for KNN; M_labels for output "
-"it reads the labels of test images and creates the 784 dim. space,"
-"in which the points from test_tuple will be entered."
-" then the KNN using N nighbours  with the points from test_tuple will be performed"
-"output - M_labels of test_tuple"
-
 
 def knn_sk(test_tuple, train_tuple, n_neighbours, m_labels):
+    # the function takes two tuples: train and test; N_neighbours for KNN; M_labels for output
+    # it reads the labels of test images and creates the 784 dim. space,
+    # in which the points from test_tuple will be entered.
+    #  then the KNN using N neighbours  with the points from test_tuple will be performed
+    # output - M_labels of test_tuple
+
     train_labels = list()
     test_pred = list()
     for i in range(len(train_tuple[0])):
         train_labels.append(train_tuple[0][i].label)
-    #print (train_lists[:10][:10])
+    # print (train_lists[:10][:10])
     # print (train_labels[:10])
     knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=n_neighbours).fit(train_tuple[1], train_labels)
     # print(knn.predict(test_list[:10]))
@@ -27,39 +24,3 @@ def knn_sk(test_tuple, train_tuple, n_neighbours, m_labels):
     # return type (train_labels), type (test_lists), type (train_lists)
 
 
-"for test purposes"
-
-training_lists = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
-print("Successfully loaded training list")
-test_lists = load_image_vectors.load_gz('../data/mnist_test.csv.gz')
-print("Successfully loaded test list")
-
-# def dim(a):
-#     if not type(a) == list:
-#         return []
-#     return [len(a)] + dim(a[0])
-
-
-# print(dim(test_lists))
-
-
-print(knn_sk(test_lists, training_lists, 3, 10))
-#print (type(training_lists))
-#def read_idx(filename):
-
-
-#     with open(filename, 'rb') as f:
-#         zero, data_type, dims = struct.unpack('>HBB', f.read(4))
-#         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
-#         return np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
-
-
-# raw_train = read_idx("../data_for_sklearn_KNN/train-images.idx3-ubyte")
-# # print(raw_train)
-# # print(raw_train.shape
-# train_data = np.reshape(raw_train,(60000,28*28))
-# train_label= read_idx("../data_for_sklearn_KNN/train-labels.idx1-ubyte")
-# # print(train_data.shape)
-# test_label = read_idx("../data_for_sklearn_KNN/t10k-labels.idx1-ubyte")
-# raw_test = read_idx("../data_for_sklearn_KNN/t10k-images.idx3-ubyte")
-# test_data = np.reshape (raw_test, (10000, 28*28))
--- src/main.py
+++ src/main.py
@@ -1,48 +1,42 @@
-import src.knn as knn
+import src.KNN_sklearn as KNN_sklearn
 import src.load_image_vectors as load_image_vectors
-import src.pca as pca
 import src.plot as plot
 
-tests_count = 0
-tests_success = 0
-
-
-def set_success_rate(prediction, test_image) -> None:
-    global tests_count, tests_success
-    if prediction == test_image.label:
-        tests_success += 1
-    tests_count += 1
-    return None
+total_number = 0
+success_number = 0
 
 
 def get_success_rate():
-    global tests_count, tests_success
-    success = float(tests_success)/float(tests_count)
+    global total_number, success_number
+    success = float(success_number) / float(total_number)
     return round(success, 4)
 
 
-def reset_rates():
-    tests_count = 0
-    tests_success = 0
-
-
-def k_value_test(k_min, k_max):
+def sklearn_k_value_test(k_min, k_max):
     # creates a list in the form of [[k1, accuracy], [k2, accuracy], ...]
     # then plots this created list as a diagram
-
+    global total_number, success_number
     k_accuracy = list()
     for k in range(k_min, k_max):
-        for i in range(1, 100):
-            predicted_digit = knn.knn_digit_prediction(test_lists[0][7], training_lists[0], k)
-            set_success_rate(predicted_digit, test_lists[i])
+        prediction_list = KNN_sklearn.knn_sk(test_lists, training_lists, k, 10)
+        for idx, prediction in enumerate(prediction_list):
+            total_number += 1
+            if prediction == test_lists[0][idx].label:
+                success_number += 1
+        print("Success rate = " + str(get_success_rate))
         k_accuracy.append([k, get_success_rate()])
         reset_rates()
     plot.plot_k_values(k_accuracy)
 
 
+def reset_rates():
+    global total_number, success_number
+    success_number = 0
+    total_number = 0
+
+
 if __name__ == '__main__':
     # number of nearest neighbors to check
-    k = 20
 
     # load training and test images
     training_lists = load_image_vectors.load_gz('../data/mnist_train.csv.gz')
@@ -50,21 +44,4 @@ if __name__ == '__main__':
     test_lists = load_image_vectors.load_gz('../data/mnist_test.csv.gz')
     print("Successfully loaded test list")
 
-    # Get reduces training and test images as tuple - reduced_images[0] is train_list, [1] is test_list without digits
-    reduced_images = pca.reduce_dimensions(training_lists[1], test_lists[1])
-    print("PCA finished successfully")
-
-    # Replace unreduced CsvImage vectors by reduced ones, for training and test images
-    for i in range(len(training_lists[0])):
-        # print(len(training_lists[0][i].image)) # for debugging
-        training_lists[0][i].image = reduced_images[0][i]
-        # print(len(training_lists[0][i].image))  # how many dimensions after reduction, slows script down
-    for i in range(len(test_lists[0])):
-        # print(len(test_lists[0][i].image))
-        test_lists[0][i].image = reduced_images[1][i]
-        # print(len(test_lists[0][i].image))  # how many dimensions after reduction, slows script down
-    print("Replaced images by reduced images")
-
-    # perform KNN for dimension reduced images (one test image)
-    predicted_digit = knn.knn_digit_prediction(test_lists[0][7], training_lists[0], k)
-    print("Predicted digit: " + str(predicted_digit) + " , expected result: " + str(test_lists[0][7].label))
+    sklearn_k_value_test(1, 5)
