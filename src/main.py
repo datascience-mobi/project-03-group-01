2613cbc8b2892dfdcf07edf81766b262dcf2e2a5
SakulV <49947770+SakulV@users.noreply.github.com>
2019-06-19 23:26:20 +0200

    moved pca variance test from plot branch to knn sklearn plot branch for merging purposes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+
 import src.KNN_sklearn as KNN_sklearn
 import src.load_image_vectors as load_image_vectors
 import src.plot as plot
@@ -35,6 +39,36 @@ def reset_rates():
     total_number = 0
 
 
+def pca_variance_analysis(input_list):
+    # calculates the retained variance for different dimensions and plots the resulting variance-matrix
+
+    scale = preprocessing.StandardScaler()
+    scale.fit(input_list)
+    input_list = scale.transform(input_list)
+    covar_matrix = PCA(n_components=784)  # we have 784 features
+    covar_matrix.fit(input_list)
+    variance = covar_matrix.explained_variance_ratio_  # calculate variance ratios
+    var = np.cumsum(np.round(variance, decimals=5) * 100)
+    print(var)  # cumulative sum of variance explained with [n] features
+    plot.plot_pca_variance(var)
+
+
+# def k_value_test(k_min, k_max):
+
+# creates a list in the form of [[k1, accuracy], [k2, accuracy], ...]
+# then plots this created list as a diagram
+
+# k_accuracy = list()
+# for k in range(k_min, k_max):
+# for i in range(10, 100):
+# sorted_distances = knn.get_sorted_distances(test_list[i], training_list)
+# predicted_digit = knn.knn_distance_prediction(sorted_distances, k)
+# knn.set_success_rate(predicted_digit, test_list[i])
+# k_accuracy.append([k, knn.get_success_rate()])
+# knn.reset_rates()
+# plot.plot_k_values(k_accuracy)
+
+
 if __name__ == '__main__':
     # number of nearest neighbors to check
 
@@ -45,3 +79,4 @@ if __name__ == '__main__':
     print("Successfully loaded test list")
 
     sklearn_k_value_test(1, 5)
+    pca_variance_analysis(test_lists[1])
