f5d9738a89e65d9bf8a1f49bbf07e54b3605d332
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-20 13:15:18 +0200

    added calculation of meta digit via mean

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import src.knn as knn
 import src.pca as pca
 import src.load_image_vectors as load_image_vectors
 import src.pickle_operations as pickle_io
+import numpy as np
+import src.image_operations as image_io
 
 tests_count = 0
 tests_success = 0
@@ -33,8 +35,8 @@ if __name__ == '__main__':
     # print("Successfully loaded test list")
     #
     # # Save created CsvImage lists in pickle files
-    # pickle_io.save_compressed_pickles(training_lists, "../data/training.dat.bz2")
-    # pickle_io.save_compressed_pickles(test_lists, "../data/test.dat.bz2")
+    # pickle_io.save_pickles(training_lists, "../data/training.dat")
+    # pickle_io.save_pickles(test_lists, "../data/test.dat")
     # print("Successfully compressed and stored pickles")
 
     # COMMENT OUT LINES ABOVE AFTER RUNNING ONCE, THEN ONLY RUN CODE BELOW
@@ -44,22 +46,30 @@ if __name__ == '__main__':
     training_lists = pickle_io.load_pickles("../data/training.dat")
     test_lists = pickle_io.load_pickles("../data/test.dat")
     print("Successfully loaded images from compressed pickle files")
+
+    for i in range(10):
+        training_array = np.asarray([csvimage.image for csvimage in training_lists if csvimage.label == i])
+
+        meta_digit = list()
+        meta_digit = (training_array.mean(axis=0))
+        image_io.draw(meta_digit)
+
+
+    # # Get reduced training and test images as tuple - reduced_images[0] is train_list, [1] is test_list without digits
+    # reduced_images = pca.reduce_dimensions(training_lists[1], test_lists[1])
+    # print("PCA finished successfully")
     #
-    # Get reduced training and test images as tuple - reduced_images[0] is train_list, [1] is test_list without digits
-    reduced_images = pca.reduce_dimensions(training_lists[1], test_lists[1])
-    print("PCA finished successfully")
-
-    # Replace unreduced CsvImage vectors by reduced ones, for training and test images
-    for i in range(len(training_lists[0])):
-        # print(len(training_lists[0][i].image)) # for debugging
-        training_lists[0][i].image = reduced_images[0][i]
-        # print(len(training_lists[0][i].image))  # how many dimensions after reduction, slows script down
-    for i in range(len(test_lists[0])):
-        # print(len(test_lists[0][i].image))
-        test_lists[0][i].image = reduced_images[1][i]
-        # print(len(test_lists[0][i].image))  # how many dimensions after reduction, slows script down
-    print("Replaced images by reduced images")
-
-    # perform KNN for dimension reduced images (one test image)
-    predicted_digit = knn.knn_digit_prediction(test_lists[0][7], training_lists[0], k)
-    print("Predicted digit: " + str(predicted_digit) + " , expected result: " + str(test_lists[0][7].label))
+    # # Replace unreduced CsvImage vectors by reduced ones, for training and test images
+    # for i in range(len(training_lists[0])):
+    #     # print(len(training_lists[0][i].image)) # for debugging
+    #     training_lists[0][i].image = reduced_images[0][i]
+    #     # print(len(training_lists[0][i].image))  # how many dimensions after reduction, slows script down
+    # for i in range(len(test_lists[0])):
+    #     # print(len(test_lists[0][i].image))
+    #     test_lists[0][i].image = reduced_images[1][i]
+    #     # print(len(test_lists[0][i].image))  # how many dimensions after reduction, slows script down
+    # print("Replaced images by reduced images")
+    #
+    # # perform KNN for dimension reduced images (one test image)
+    # predicted_digit = knn.knn_digit_prediction(test_lists[0][7], training_lists[0], k)
+    # print("Predicted digit: " + str(predicted_digit) + " , expected result: " + str(test_lists[0][7].label))
