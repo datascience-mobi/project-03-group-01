235d44a94183fbaa0e0ccd39684dbd8c1c9b9c41
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-28 23:10:51 +0200

    began calculating meta digit as best recognized test image for each label

--- src/KNN_sklearn.py
+++ src/KNN_sklearn.py
@@ -2,6 +2,7 @@ import sklearn
 import src.load_image_vectors as load_image_vectors
 import src.pickle_operations as pickle_io
 from sklearn import neighbors, metrics
+import src.knn as knn
 
 
 "the function takes two tuples: train and test; N_neighbours for KNN; M_labels for output "
@@ -23,12 +24,38 @@ def knn_sk(test_images, train_images, n_neighbours, min_index, max_index):
     """
     test_pred = list()
     knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=n_neighbours).fit([csv_image.image for csv_image in train_images], [csv_image.label for csv_image in train_images])
-    for j in range(min_index, max_index):
+    print("Finished KNeighborsClassifier") # takes most time
+    for j in range(len(test_images)):
         test_pred.append([csv_image.image for csv_image in test_images][j])
-        print(j)
-    test_prediction = knn.predict(test_pred).tolist()
+        # print(j)
+    test_prediction = knn.predict_proba(test_pred).tolist()
+    # print(knn.predict_proba(test_pred))
     test_prediction = [list(a) for a in zip([j for j in range(min_index, max_index)], test_prediction)]
     return test_prediction
+
+
+def get_total_index(image_list, digit, local_index):
+    j = -1
+    for i, img in enumerate(image_list):
+        print(f"{i}, {img.label}")
+        if img.label == digit:
+            j += 1
+        if j == local_index:
+            print(f"success: i = {i}")
+            return i
+
+
+def get_most_unique_image(preds):
+    max_accuracy = 0.0
+    max_label = -1
+    for idx, pred in enumerate(preds):
+        # print(pred[1][5])
+        if pred[1][5] > max_accuracy:
+            max_accuracy = pred[1][5]
+            max_label = idx
+    true_label = get_total_index(test_lists, 5, max_label)
+    print(f"max_accuracy: {max_accuracy}, max_label: {max_label}, true label: {true_label}")
+    return true_label
 
 
 "for test purposes"
@@ -37,4 +64,14 @@ if __name__ == '__main__':
     print("Successfully loaded training list")
     test_lists = pickle_io.load_pickles('../data/test.dat')
     print("Successfully loaded test list")
-    print(knn_sk(test_lists, training_lists, 3, 10, 15))
+    # print(training_lists[get_total_index(5, 623)].image)
+    # print([train for train in training_lists if train.label == 5][624].image)
+    # print(training_lists[get_total_index(5, 623)].image == [train for train in training_lists if train.label == 5][623].image)
+    best_images = list()
+    for i in range(10):
+        predictions = (knn_sk([test for test in test_lists if test.label == i], training_lists, 500, 0, 5999))
+        best_images.append(get_most_unique_image(predictions))
+    print(best_images)
+    with open('best_digits.txt', 'w') as the_file:
+        for index in best_images:
+            the_file.write('index')
