90df834581644a4cab3d0e89ab0964557439b68b
g1YC31aA <lennartmobil2016@gmail.com>
2019-07-01 00:39:17 +0200

    tested version to determine the most clearly recognized image for each digit out of the test images

--- src/KNN_sklearn.py
+++ src/KNN_sklearn.py
@@ -5,6 +5,7 @@ from sklearn import neighbors, metrics
 import src.knn as knn
 
 
+
 "the function takes two tuples: train and test; N_neighbours for KNN; M_labels for output "
 "it reads the labels of test images and creates the 784 dim. space,"
 "in which the points from test_tuple will be entered."
@@ -22,22 +23,29 @@ def knn_sk(test_images, train_images, n_neighbours, min_index, max_index):
     :param max_index: lowest test image number NOT to perform the knn for
     :return: 2d list of type [[10, 0], [11, 6]] meaning [test image number, prediction]
     """
-    test_pred = list()
+    # test_pred = list()
     knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=n_neighbours).fit([csv_image.image for csv_image in train_images], [csv_image.label for csv_image in train_images])
     print("Finished KNeighborsClassifier") # takes most time
-    for j in range(len(test_images)):
-        test_pred.append([csv_image.image for csv_image in test_images][j])
-        # print(j)
-    test_prediction = knn.predict_proba(test_pred).tolist()
-    # print(knn.predict_proba(test_pred))
-    test_prediction = [list(a) for a in zip([j for j in range(min_index, max_index)], test_prediction)]
-    return test_prediction
+    # for j in range(len(test_images)):
+    #     test_pred.append([csv_image.image for csv_image in test_images][j])
+    #     # print(j)
+    # print(test_pred == test_images)
+    test_predictions = list()
+    for i in range(10):
+        print(f"inside knn: {i}")
+
+        test_prediction = knn.predict_proba([csv_image.image for csv_image in test_images if csv_image.label == i]).tolist()
+        # print(knn.predict_proba(test_pred))
+        test_prediction = [list(a) for a in zip([j for j in range(len(test_images))], test_prediction)]
+        print(test_prediction)
+        test_predictions.append(test_prediction)
+    return test_predictions
 
 
 def get_total_index(image_list, digit, local_index):
     j = -1
     for i, img in enumerate(image_list):
-        print(f"{i}, {img.label}")
+        # print(f"{i}, {img.label}")
         if img.label == digit:
             j += 1
         if j == local_index:
@@ -45,15 +53,21 @@ def get_total_index(image_list, digit, local_index):
             return i
 
 
-def get_most_unique_image(preds):
+def get_most_unique_image(preds, label):
+    """
+    determines image that got best recognized as what digit it is
+    :param preds: list of probabilities
+    :param label:
+    :return:
+    """
     max_accuracy = 0.0
     max_label = -1
     for idx, pred in enumerate(preds):
         # print(pred[1][5])
-        if pred[1][5] > max_accuracy:
-            max_accuracy = pred[1][5]
+        if pred[1][label] > max_accuracy:
+            max_accuracy = pred[1][label]
             max_label = idx
-    true_label = get_total_index(test_lists, 5, max_label)
+    true_label = get_total_index(test_lists, label, max_label)
     print(f"max_accuracy: {max_accuracy}, max_label: {max_label}, true label: {true_label}")
     return true_label
 
@@ -68,10 +82,13 @@ if __name__ == '__main__':
     # print([train for train in training_lists if train.label == 5][624].image)
     # print(training_lists[get_total_index(5, 623)].image == [train for train in training_lists if train.label == 5][623].image)
     best_images = list()
+    all_predictions = (knn_sk(test_lists, training_lists, 500, 0, 5999))
+    pickle_io.save_pickles(all_predictions, "../data/skknnproba.dat")
     for i in range(10):
-        predictions = (knn_sk([test for test in test_lists if test.label == i], training_lists, 500, 0, 5999))
-        best_images.append(get_most_unique_image(predictions))
-    print(best_images)
+        print(f"i: {i}")
+        # predictions = (knn_sk([test for test in test_lists if test.label == i], training_lists, 500, 0, 5999))
+        best_images.append(get_most_unique_image(all_predictions[i], i))
+    print(f"Best images: {best_images}")
     with open('best_digits.txt', 'w') as the_file:
         for index in best_images:
             the_file.write('index')
