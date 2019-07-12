c067faacb1979ebf2079113937a1aeca0c213ec3
g1YC31aA <lennartmobil2016@gmail.com>
2019-07-12 21:14:49 +0200

    merging

--- src/main.py
+++ src/main.py
@@ -28,8 +28,22 @@ if __name__ == '__main__':
     test_lists = pickle_io.load_pickles("../data/test.dat")
     print("Successfully loaded images from pickle files")
 
+
+    # runs the k_accuracy test with 10000 images between the chosen k values (k_min, k_max) > then plots the result
+    # plot.k_accuracy_test(training_lists, test_lists, 1, 4)  # saves the result as k_accuracy2 to avoid time wasted
+    plot.plot_k_accuracy(pickle_io.load_pickles("k_accuracy.dat"))
+
+    # runs the pca_variance_analysis and plots it
+    plot.pca_variance_analysis([csv_image.image for csv_image in test_lists])
+
+    # Get reduces training and test images as tuple - reduced_images[0] is train_list, [1] is test_list without digits
+    reduced_images = pca.reduce_dimensions([csv_image.image for csv_image in training_lists],
+                                           [csv_image.image for csv_image in test_lists], 784)
+    print("PCA finished successfully")
+    pca.plot_inverse_transforms(reduced_images[2], reduced_images[1], reduced_images[3])
+
     # # Get reduces training and test images as tuple - reduced_images[0] is train_list, [1] is test_list without digits
-    # reduced_images = pca.reduce_dimensions([csv_image.image for csv_image in training_lists], [csv_image.image for csv_image in test_lists], 784)
+    reduced_images = pca.reduce_dimensions([csv_image.image for csv_image in training_lists], [csv_image.image for csv_image in test_lists], 78)
     # print("PCA finished successfully")
     #
     # pca.plot_inverse_transforms(reduced_images[2], reduced_images[1], reduced_images[3])
@@ -56,13 +70,6 @@ if __name__ == '__main__':
     # perform KNN for dimension reduced images (one test image)
     predicted_digit = knn.knn_digit_prediction(test_lists[7], training_lists, k)
     print("Predicted digit: " + str(predicted_digit) + " , expected result: " + str(test_lists[7].label))
-
-    # runs the k_accuracy test with 10000 images between the chosen k values (k_min, k_max) > then plots the result
-    plot.k_accuracy_test(training_lists, test_lists, 1, 4)  # saves the result as k_accuracy2 to avoid time wasted
-    plot.plot_k_accuracy(pickle_io.load_pickles("k_accuracy.dat"))
-
-    # runs the pca_variance_analysis and plots it
-    plot.pca_variance_analysis([csv_image.image for csv_image in test_lists])
 
     # performs pca_accuracy_test, then plots it
     # plot.pca_accuracy_test(test_lists, training_lists, 1)  # saves as pca_accuracy2 to avoid time wasted
