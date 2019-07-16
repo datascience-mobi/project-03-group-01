c24786953034377a6f4203041b16c42684e05bca
g1YC31aA <lennartmobil2016@gmail.com>
2019-07-16 18:45:27 +0200

    adjusted for testing

--- src/drawing_canvas.py
+++ src/drawing_canvas.py
@@ -1,14 +1,14 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-from kivy.graphics import Color, Line
-from kivy.core.window import Window
-from kivy.config import Config
-from PIL import Image, ImageFilter
-import kivy.uix.image as image
-from kivy.uix.button import Button
+# from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.widget import Widget
+# from kivy.graphics import Color, Line
+# from kivy.core.window import Window
+# from kivy.config import Config
+# import kivy.uix.image as image
+# from kivy.uix.button import Button
+# from random import randint
 import image_operations
-from random import randint
+from PIL import Image, ImageFilter
 
 
 def image_prepare(path) -> list:
@@ -54,111 +54,111 @@ def image_prepare(path) -> list:
     return mnist_image
 
 
-# Canvas widget for drawing window
-class MyPaintWidget(Widget):
-
-    def __init__(self, **kwargs):
-        """
-        Setting up the key recognition and the drawing widget within the window
-        :param kwargs: TODO probably has to be kept because it's a prebuild kivy function
-        """
-        super(MyPaintWidget, self).__init__(**kwargs)
-        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self._on_keyboard_down)
-
-    def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
-
-    def _on_keyboard_down(self, keyboard, key_code, text, modifiers) -> bool:
-        """
-        Specifying key events
-        :param keyboard: not used TODO probably has to be kept because it's a prebuild kivy function
-        :param key_code: which key is pressed
-        :param text: not used TODO probably has to be kept because it's a prebuild kivy function
-        :param modifiers: not used TODO probably has to be kept because it's a prebuild kivy function
-        :return: True if no error occured
-        """
-        if key_code[1] == 'enter':  # Apply drawn canvas on enter key pressed
-            print(str("Leaving canvas, saving entered digit .."))
-            MyPaintApp.get_running_app().stop()
-            MyPaintWidget.export_to_png(self, "test.png")
-        elif key_code[1] == 'escape':  # Reset Canvas to restart drawing on escape key pressed
-            print(str("Resetting canvas .."))
-            self.canvas.clear()
-        return True
-
-    def on_touch_down(self, touch) -> None:
-        """
-        Begins line when left mouse button is first pressed
-        :param touch: contains cursor coordinates amongst others
-        :return: None
-        """
-        with self.canvas:
-            Color(1, 1, 1)
-            touch.ud['line'] = Line(points=(touch.x, touch.y), width=35)
-
-    def on_touch_move(self, touch) -> None:
-        """
-        Attaches coordinates to line when pressed mouse is moved
-        :param touch: contains cursor coordinates amongst others
-        :return: None
-        """
-        touch.ud['line'].points += [touch.x, touch.y]
+# # Canvas widget for drawing window
+# class MyPaintWidget(Widget):
+#
+#     def __init__(self, **kwargs):
+#         """
+#         Setting up the key recognition and the drawing widget within the window
+#         :param kwargs: TODO probably has to be kept because it's a prebuild kivy function
+#         """
+#         super(MyPaintWidget, self).__init__(**kwargs)
+#         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+#         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+#
+#     def _keyboard_closed(self):
+#         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+#         self._keyboard = None
+#
+#     def _on_keyboard_down(self, keyboard, key_code, text, modifiers) -> bool:
+#         """
+#         Specifying key events
+#         :param keyboard: not used TODO probably has to be kept because it's a prebuild kivy function
+#         :param key_code: which key is pressed
+#         :param text: not used TODO probably has to be kept because it's a prebuild kivy function
+#         :param modifiers: not used TODO probably has to be kept because it's a prebuild kivy function
+#         :return: True if no error occured
+#         """
+#         if key_code[1] == 'enter':  # Apply drawn canvas on enter key pressed
+#             print(str("Leaving canvas, saving entered digit .."))
+#             MyPaintApp.get_running_app().stop()
+#             MyPaintWidget.export_to_png(self, "test.png")
+#         elif key_code[1] == 'escape':  # Reset Canvas to restart drawing on escape key pressed
+#             print(str("Resetting canvas .."))
+#             self.canvas.clear()
+#         return True
+#
+#     def on_touch_down(self, touch) -> None:
+#         """
+#         Begins line when left mouse button is first pressed
+#         :param touch: contains cursor coordinates amongst others
+#         :return: None
+#         """
+#         with self.canvas:
+#             Color(1, 1, 1)
+#             touch.ud['line'] = Line(points=(touch.x, touch.y), width=35)
+#
+#     def on_touch_move(self, touch) -> None:
+#         """
+#         Attaches coordinates to line when pressed mouse is moved
+#         :param touch: contains cursor coordinates amongst others
+#         :return: None
+#         """
+#         touch.ud['line'].points += [touch.x, touch.y]
 
 
 # TODO probably creates the canvas window
-class MyPaintApp(App):
+# class MyPaintApp(App):
+#
+#     def __init__(self, title):
+#         App.__init__(self)
+#         self.title="Please draw a "+str(title)
+#
+#     def build(self):
+#         super_box = BoxLayout(orientation='vertical')
+#         super_box.add_widget(MyPaintWidget())
+#         # button = Button(text="hi", size_hint=(0.1, .1))
+#         # super_box.add_widget(button)
+#         try:
+#             # load the image
+#             picture = image(source="mnist.png")
+#             # add to the main field
+#             super_box.add_widget(picture)
+#         except Exception as e:
+#             print('Pictures: Unable to load <%s>' % "fname.png")
+#
+#         return super_box
 
-    def __init__(self, title):
-        App.__init__(self)
-        self.title="Please draw a "+str(title)
 
-    def build(self):
-        super_box = BoxLayout(orientation='vertical')
-        super_box.add_widget(MyPaintWidget())
-        # button = Button(text="hi", size_hint=(0.1, .1))
-        # super_box.add_widget(button)
-        try:
-            # load the image
-            picture = image(source="mnist.png")
-            # add to the main field
-            super_box.add_widget(picture)
-        except Exception as e:
-            print('Pictures: Unable to load <%s>' % "fname.png")
-
-        return super_box
-
-
-def drawn_image(digit) -> list:
-    """
-    Initializes draw_canvas
-    :return: mnist compatible drawn image
-    """
-    # Sets window parameters
-    Config.set('graphics', 'resizable', 'false')  # 0 being off 1 being on as in true/false
-    Config.set('graphics', 'width', '560')
-    Config.set('graphics', 'height', '560')
-    Config.write()
-
-    # Runs drawing window
-    MyPaintApp(digit).run()
-
-    # Transform saved image to mnist style image
-    x = image_prepare('test.png')  # path must fit path in MyPaintWidget.on_keyboard_down
-
-    # show and save mnist style input image
-    image_operations.draw(x)
-    image_operations.save("mnist.png", x)
-
-    # Control: does mnist style image actually fit mnist size?
-    print(len(x))  # mnist IMAGES are 28x28=784 pixels
-    return x
+# def drawn_image(digit) -> list:
+#     """
+#     Initializes draw_canvas
+#     :return: mnist compatible drawn image
+#     """
+#     # Sets window parameters
+#     Config.set('graphics', 'resizable', 'false')  # 0 being off 1 being on as in true/false
+#     Config.set('graphics', 'width', '560')
+#     Config.set('graphics', 'height', '560')
+#     Config.write()
+#
+#     # Runs drawing window
+#     MyPaintApp(digit).run()
+#
+#     # Transform saved image to mnist style image
+#     x = image_prepare('test.png')  # path must fit path in MyPaintWidget.on_keyboard_down
+#
+#     # show and save mnist style input image
+#     image_operations.draw(x)
+#     image_operations.save("mnist.png", x)
+#
+#     # Control: does mnist style image actually fit mnist size?
+#     print(len(x))  # mnist IMAGES are 28x28=784 pixels
+#     return x
 
 
 if __name__ == '__main__':
     # For debugging purposes -> directly call drawing functions without further recognition
-    random_digit = randint(0, 9)
-    print(random_digit)
-    y = drawn_image(random_digit)
-
+    # random_digit = randint(0, 9)
+    # print(random_digit)
+    # y = drawn_image(random_digit)
+    image_operations.draw(image_prepare("mnist.png"))
--- src/main.py
+++ src/main.py
@@ -59,7 +59,8 @@ if __name__ == '__main__':
     # get drawn image, adjusted for mnist
     random_digit = randint(0, 9)
     print(random_digit)
-    test_vector = drawing_canvas.drawn_image(random_digit)
+    # test_vector = drawing_canvas.drawn_image(random_digit)
+    test_vector = drawing_canvas.image_prepare("mnist.png")
 
     # insert label to fit test_vector for CsvImage class
     test_vector.insert(0, -1)
