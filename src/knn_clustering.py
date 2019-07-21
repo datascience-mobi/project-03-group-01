3c4490cd7153a205d9d37b86f046c7fda1905bd7
g1YC31aA <lennartmobil2016@gmail.com>
2019-07-21 20:55:49 +0200

    added comments

--- src/knn_clustering.py
+++ src/knn_clustering.py
@@ -1,18 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import AgglomerativeClustering
 import src.KNN_sklearn as KNN_sklearn
 from src import pca
 
 
 def get_mispredictions(train_list, test_list):
-    reds = pca.reduce_dimensions([csv_image.image for csv_image in train_list], [csv_image.image for csv_image in test_list], 78)
+    """
 
-    predictions = KNN_sklearn.knn_sk(reds[0], reds[1], [csv_image.label for csv_image in train_list], 3, 0, 10000)
+    :param train_list:
+    :param test_list:
+    :return: None
+    """
+    # Runs PCA for optimal number of dimensions
+    reduced_images = pca.reduce_dimensions([csv_image.image for csv_image in train_list], [csv_image.image for csv_image in test_list], 78)
+
+    # Runs KNN for all test images and optimal k number
+    predictions = KNN_sklearn.knn_sk(reduced_images[0], reduced_images[1], [csv_image.label for csv_image in train_list], 3, 0, 10000)
+
+    # Creates table with number of misrecognitions
     table = [[0 for i in range(10)] for j in range(10)]
     for pred in predictions:
         if not pred[1] == test_list[pred[0]].label:
             table[pred[1]][test_list[pred[0]].label] += 1
+    # Transforms
     table = np.asarray(table)
     im = plt.imshow(table.reshape(10, 10), cmap='hot', interpolation='nearest')
     plt.xlabel("Correct label")
